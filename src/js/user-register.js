330ec368e994cec7d74671e9fc022f603db08a33
adrianminnaar <adrianminaar29@gmail.com>
2023-03-12 22:00:23 +0200

    update

--- src/js/user-register.js
+++ src/js/user-register.js
@@ -13,8 +13,12 @@
     // Get reference to all elements in userList tab
     const userTabs = document.getElementsByClassName("users");
     console.log(userTabs);
+
     const userTabs1 = userTabs[0]
     const userTabs2 = userTabs[1]
+
+    //switching between userTabs and registerTab
+
 document.getElementById("register-tab-button").addEventListener('click',function() {
     userTabs[0].style.display = "block";
     registerTab[0].style.display = "none";
@@ -42,8 +46,8 @@ console.log(navUsers)
 /* --- Form field references --- */
 
     // code here...
-const formFields = document.getElementsByClassName("register");
-console.log(formFields)
+const regForm = document.getElementsByClassName("register")[1];
+console.log(regForm)
 
 /* --- Table list reference --- */
 
@@ -57,14 +61,52 @@ console.log(userTable)
 ---------------------------------------------------- */
 
     // code here...
-const userFname= document.getElementById("firstname").value;
-const userLname = document.getElementById("lastname").value;
-const username = document.getElementById("username").value;
-const userMail = document.getElementById("email").value;
-const passWord = document.getElementById("password").value;
+    //empty array
+    var users = [ " "," "," "," "," "]
 
+    //switching back to Register Tab when submiting
 
+    document.getElementById("register-submit").addEventListener('click',function submit() {
+        userTabs[0].style.display = "block";
+        registerTab[0].style.display = "none";
+        userTabs[1].style.display = "none";
+        registerTab[1].style.display = "block";
+    
+    })
+    
+//Creating new users
 
+    function create_users(){
+        
+    const userFname= document.getElementById("firstname").value;
+    const userLname = document.getElementById("lastname").value;
+    const username = document.getElementById("username").value;
+    const userMail = document.getElementById("email").value;
+    const passWord = document.getElementById("password").value;
+    
+    const new_User = ([userFname] + " "+ [userLname] + " " + [username] + " "+ [userMail] + " " + [passWord]);
+    new_User = users
+    
+    
+    for (let i = 0; i < users.length; i++) {
+        const user_List = document.getElementById("user-list");
+        const newusers = users[i];
+        const li = create_UserElement("user");
+        user_List=append(li);
+        console.log(users)
+    }
+
+    
+
+};
+function create_UserElement(user) {
+    
+}
+ 
+   
+
+  
+  
 /* ----------------------------------------------------
     Event Listenters and Interactivity
 ---------------------------------------------------- */
